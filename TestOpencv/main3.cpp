c0d6feca841c1b798ee7d5407a550b4a24a951ce
k x <229764397@qq.com>
2020-10-13 19:56:30 +0800

    10.13

--- TestOpencv/main3.cpp
+++ TestOpencv/main3.cpp
@@ -8,59 +8,394 @@ using namespace cv;
 
 using namespace std;
 
-//凸包
-Mat src, src_gray, dst;
-int threshold_value = 100;
-int threshold_max = 255;
-const char* output_win = "convex hull demo";
-void Threshold_Callback(int, void*);
-RNG rng(12345);
-int main(int argc, char** argv) {
-	src = imread("C:/Users/xk/Desktop/hand2.png");
-	if (!src.data) {
+
+//基于距离变换与分水岭的图像分割
+int main()
+{
+	char input_win[] = "input image";
+	char watershed_win[] = "watershed segmentation demo";
+	Mat src = imread("C:/Users/xk/Desktop/puke.png");
+	// Mat src = imread("D:/kuaidi.jpg");
+	if (src.empty()) {
 		printf("could not load image...\n");
 		return -1;
 	}
-	const char* input_win = "input image";
 	namedWindow(input_win, CV_WINDOW_AUTOSIZE);
-	namedWindow(output_win, CV_WINDOW_NORMAL);
-	const char* trackbar_label = "Threshold : ";
+	imshow(input_win, src);
+	// 1. change background
+	for (int row = 0; row < src.rows; row++) {
+		for (int col = 0; col < src.cols; col++) {
+			if (src.at<Vec3b>(row, col) == Vec3b(255, 255, 255)) {
+				src.at<Vec3b>(row, col)[0] = 0;
+				src.at<Vec3b>(row, col)[1] = 0;
+				src.at<Vec3b>(row, col)[2] = 0;
+			}
+		}
+	}
+	namedWindow("black background", CV_WINDOW_AUTOSIZE);
+	imshow("black background", src);
 
-	cvtColor(src, src_gray, CV_BGR2GRAY);
-	blur(src_gray, src_gray, Size(3, 3), Point(-1, -1), BORDER_DEFAULT);
-	imshow(input_win, src_gray);
+	// sharpen
+	Mat kernel = (Mat_<float>(3, 3) << 1, 1, 1, 1, -8, 1, 1, 1, 1);
+	Mat imgLaplance;
+	Mat sharpenImg = src;
+	filter2D(src, imgLaplance, CV_32F, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	src.convertTo(sharpenImg, CV_32F);
+	Mat resultImg = sharpenImg - imgLaplance;
 
-	createTrackbar(trackbar_label, output_win, &threshold_value, threshold_max, Threshold_Callback);
-	Threshold_Callback(0, 0);
+	resultImg.convertTo(resultImg, CV_8UC3);
+	imgLaplance.convertTo(imgLaplance, CV_8UC3);
+	imshow("sharpen image", resultImg);
+
+	//convert to binary
+	Mat binaryImg;
+	cvtColor(resultImg, resultImg, CV_BGR2GRAY);
+	threshold(resultImg, binaryImg, 40, 255, THRESH_BINARY | THRESH_OTSU);
+	imshow("binary image", binaryImg);
+
+	Mat distImg;
+	distanceTransform(binaryImg, distImg, DIST_L1, 3, 5);
+	normalize(distImg, distImg, 0, 1, NORM_MINMAX);
+	imshow("distance result", distImg);
+
+	//binary again	
+	threshold(distImg, distImg, 0.4, 1, THRESH_BINARY);
+	Mat k1 = Mat::ones(3, 3, CV_8UC1);
+	erode(distImg, distImg, k1,Point(-1,-1));
+	imshow("distance binary image", distImg);
+
+	//markers
+	Mat dist_8u;
+	distImg.convertTo(dist_8u, CV_8U);
+	vector<vector<Point>> contours;
+	findContours(dist_8u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	
+	//create markers
+	Mat markers = Mat::zeros(src.size(), CV_32SC1);
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		drawContours(markers, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i) + 1),-1);
+	}
+	circle(markers, Point(5, 5), 3, Scalar(255, 255, 255), -1);
+	imshow("my markers", markers*1000);
+
+	//perform watershed
+	watershed(src, markers);
+	Mat mark = Mat::zeros(markers.size(), CV_8UC1);
+	markers.convertTo(mark, CV_8UC1);
+	bitwise_not(mark, mark, Mat());
+	imshow("watershed image", mark);
+
+	//generate random color
+	vector<Vec3b> colors;
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		int r = theRNG().uniform(0, 255);
+		int g = theRNG().uniform(0, 255);
+		int b = theRNG().uniform(0, 255);
+		colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+	}
+
+	//fill with color and display final result
+	Mat dst = Mat::zeros(markers.size(), CV_8UC3);
+	for (int row = 0; row < markers.rows; row++)
+	{
+		for (int col = 0; col < markers.cols; col++)
+		{
+			int index = markers.at<int>(row, col);
+			if (index > 0 && index <= static_cast<int>(contours.size()))
+			{
+				dst.at<Vec3b>(row, col) = colors[index - 1];
+			}
+			else
+			{
+				dst.at<Vec3b>(row, col) = Vec3b(0,0,0);
+			}
+		}
+	}
+	imshow("Final Result", dst);
+
 	waitKey(0);
 	return 0;
 }
 
-void Threshold_Callback(int, void*) {
-	Mat bin_output;
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierachy;
 
-	threshold(src_gray, bin_output, threshold_value, threshold_max, THRESH_BINARY);
-	findContours(bin_output, contours, hierachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	vector<vector<Point>> convexs(contours.size());
-	for (size_t i = 0; i < contours.size(); i++) {
-		convexHull(contours[i], convexs[i], false, true);//计算出图像的凸包，根据图像的轮廓点，通过函数convexhull转化成凸包的点点坐标
-	}
+//点多边形测试
+//int main()
+//{
+//	const int r = 100;
+//	Mat src = Mat::zeros(r * 4, r * 4, CV_8UC1);
+//
+//	vector<Point2f> vert(6);
+//	vert[0] = Point(3 * r / 2, static_cast<int>(1.34 * r));
+//	vert[1] = Point(1 * r, 2 * r);
+//	vert[2] = Point(3 * r / 2, static_cast<int>(2.866 * r));
+//	vert[3] = Point(5 * r / 2, static_cast<int>(2.866 * r));
+//	vert[4] = Point(3 * r, 2 * r);
+//	vert[5] = Point(5 * r / 2, static_cast<int>(1.34 * r));
+//
+//	for (int i = 0; i < 6; i++)
+//	{
+//		line(src, vert[i], vert[(i + 1) % 6], Scalar(255), 3, 8, 0);
+//	}
+//
+//	vector<vector<Point>> contours;
+//	vector<Vec4i> hierachy;
+//	Mat csrc;
+//	src.copyTo(csrc);
+//	findContours(csrc, contours, hierachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(-1, -1));
+//	Mat raw_dist = Mat::zeros(csrc.size(), CV_32FC1);
+//	for (int row = 0; row < raw_dist.rows; row++)
+//	{
+//		for (int col = 0; col < raw_dist.cols; col++)
+//		{
+//			double dist = pointPolygonTest(contours[0], Point2f(static_cast<float>(col), static_cast<float>(row)), true);
+//			raw_dist.at<float>(row, col) = static_cast<float>(dist);
+//		}
+//	}
+//
+//	double minValue, maxValue;
+//	minMaxLoc(raw_dist, &minValue, &maxValue, 0, 0, Mat());
+//	Mat drawImg = Mat::zeros(src.size(), CV_8UC3);
+//	for (int row = 0; row < drawImg.rows; row++)
+//	{
+//		for (int col = 0; col < drawImg.cols; col++)
+//		{
+//			float dist = raw_dist.at<float>(row, col);
+//			if (dist > 0)
+//			{
+//				drawImg.at<Vec3b>(row, col)[0] = (uchar)(abs(1.0 - dist / maxValue) * 255);
+//			}
+//			else if (dist < 0)
+//			{
+//				drawImg.at<Vec3b>(row, col)[2] = (uchar)(abs(1.0 - dist / minValue) * 255);
+//			}
+//			else
+//			{
+//				drawImg.at<Vec3b>(row, col)[0] = (uchar)(abs(255 - dist));
+//				drawImg.at<Vec3b>(row, col)[1] = (uchar)(abs(255 - dist));
+//				drawImg.at<Vec3b>(row, col)[2] = (uchar)(abs(255 - dist));
+//			}
+//		}
+//	}
+//
+//	const char* output_win = "point polygon test demo";
+//	char input_win[] = "input image";
+//	namedWindow(input_win, CV_WINDOW_AUTOSIZE);
+//	namedWindow(output_win, CV_WINDOW_AUTOSIZE);
+//
+//	imshow(input_win, src);
+//	imshow(output_win, drawImg);
+//
+//	waitKey(0);
+//	return 0;
+//}
 
-	// 绘制
-	dst = Mat::zeros(src.size(), CV_8UC3);
-	vector<Vec4i> empty(0);
-	for (size_t k = 0; k < contours.size(); k++) {
-		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-		drawContours(dst, contours, k, color, 2, LINE_8, hierachy, 0, Point(0, 0));
-		drawContours(dst, convexs, k, color, 2, LINE_8, empty, 0, Point(0, 0));
-	}
-	imshow(output_win, dst);
 
-	return;
-}
+
+
+
+
+//图像矩
+//Mat src, gray_src;
+//int threshold_value = 80;
+//int threshold_max = 255;
+//const char* output_win = "image moents demo";
+//RNG rng(12345);
+//void Demo_Moments(int, void*);
+//int main()
+//{
+//	src = imread("C:/Users/xk/Desktop/circles.png");
+//	if (!src.data) {
+//		printf("could not load image...\n");
+//		return -1;
+//	}
+//	cvtColor(src, gray_src, CV_BGR2GRAY);
+//	GaussianBlur(gray_src, gray_src, Size(3, 3), 0, 0);
+//
+//	char input_win[] = "input image";
+//	namedWindow(input_win, CV_WINDOW_AUTOSIZE);
+//	namedWindow(output_win, CV_WINDOW_AUTOSIZE);
+//	imshow(input_win, src);
+//
+//	createTrackbar("Threshold Value:", output_win, &threshold_value, threshold_max, Demo_Moments);
+//	Demo_Moments(0, 0);
+//
+//	waitKey(0);
+//	return 0;
+//}
+//void Demo_Moments(int, void*)
+//{
+//	Mat canny_output;
+//	vector<vector<Point>> contours;
+//	vector<Vec4i> hirerachy;
+//	
+//	Canny(gray_src, canny_output, threshold_value, threshold_value * 2, 3, false);
+//	findContours(canny_output, contours, hirerachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+//
+//	vector<Moments> contours_moments(contours.size());
+//	vector<Point2f> ccs(contours.size());
+//	for (size_t i = 0; i < contours.size(); i++)
+//	{
+//		contours_moments[i] = moments(contours[i]);
+//		ccs[i] = Point(static_cast<float>(contours_moments[i].m10 / contours_moments[i].m00), static_cast<float>(contours_moments[i].m01 / contours_moments[i].m00));
+//	}
+//
+//	Mat drawImg;//= Mat::zeros(src.size(), CV_8UC3);
+//	src.copyTo(drawImg);
+//	for (size_t i = 0; i < contours.size(); i++)
+//	{
+//		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//		printf("center point x : %.2f  y : %.2f\n", ccs[i].x, ccs[i].y);
+//		printf("contours %d area: %.2f  arc length: %.2f\n", i, contourArea(contours[i]), arcLength(contours[i],true));
+//		drawContours(drawImg, contours, i, color, 2, 8, hirerachy, 0, Point(0, 0));
+//		circle(drawImg, ccs[i], 2, color, 2, 8);
+//	}
+//
+//	imshow(output_win, drawImg);
+//	return;
+//
+//}
+
+
+
+//轮廓周围绘制矩形框和圆形框
+//Mat src, gray_src, drawImg;
+//int threshold_v = 170;
+//int threshold_max = 255;
+//const char* output_win = "rectangle-demo";
+//RNG rng(12345);
+//void Contours_Callback(int, void*);
+//int main()
+//{
+//	src = imread("C:/Users/xk/Desktop/reqiqiu.png");
+//	if (!src.data) {
+//		printf("could not load image...\n");
+//		return -1;
+//	}
+//	cvtColor(src, gray_src, CV_BGR2GRAY);
+//	blur(gray_src, gray_src, Size(3, 3), Point(-1, -1));
+//
+//	const char* source_win = "input image";
+//	namedWindow(source_win, CV_WINDOW_AUTOSIZE);
+//	namedWindow(output_win, CV_WINDOW_AUTOSIZE);
+//	imshow(source_win, src);
+//
+//	createTrackbar("Threshold Value:", output_win, &threshold_v, threshold_max, Contours_Callback);
+//	Contours_Callback(0, 0);
+//
+//	waitKey(0);
+//	return 0;
+//}
+//void Contours_Callback(int, void*)
+//{
+//	Mat binary_output;
+//	vector<vector<Point>> contours;
+//	vector<Vec4i> hierachy;
+//	threshold(gray_src, binary_output, threshold_v, threshold_max, THRESH_BINARY);
+//	findContours(binary_output, contours, hierachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(-1, -1));
+//
+//	vector<vector<Point>> contours_ploy(contours.size());
+//	vector<Rect> ploy_rects(contours.size());
+//	vector<Point2f> ccs(contours.size());
+//	vector<float> radius(contours.size());
+//
+//	vector<RotatedRect> minRects(contours.size());
+//	vector<RotatedRect> myellipse(contours.size());
+//
+//	for (size_t i = 0; i < contours.size(); i++)
+//	{
+//		approxPolyDP(Mat(contours[i]), contours_ploy[i], 3, true);
+//		ploy_rects[i] = boundingRect(contours_ploy[i]);
+//		minEnclosingCircle(contours_ploy[i], ccs[i], radius[i]);
+//		if (contours_ploy[i].size() > 5)
+//		{
+//			myellipse[i] = fitEllipse(contours_ploy[i]);
+//			minRects[i] = minAreaRect(contours_ploy[i]);
+//		}
+//		
+//	}
+//
+//	//draw it
+//	drawImg = Mat::zeros(src.size(), src.type());//src.copyTo(drawImg);
+//	Point2f pts[4];
+//	for (size_t i = 0; i < contours.size(); i++)
+//	{
+//		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//		//rectangle(drawImg, ploy_rects[i], color, 2, 8);
+//		//circle(drawImg, ccs[i], radius[i], color, 2, 8);
+//
+//		if (contours_ploy[i].size() > 5)
+//		{
+//			ellipse(drawImg, myellipse[i], color, 1, 8);
+//			minRects[i].points(pts);
+//			for (int r = 0; r < 4; r++)
+//			{
+//				line(drawImg, pts[r], pts[(r + 1) % 4], color, 1, 8);
+//			}
+//		}
+//		
+//	}
+//
+//	imshow(output_win, drawImg);
+//	return;
+//}
+
+
+//凸包
+//Mat src, src_gray, dst;
+//int threshold_value = 100;
+//int threshold_max = 255;
+//const char* output_win = "convex hull demo";
+//void Threshold_Callback(int, void*);
+//RNG rng(12345);
+//int main(int argc, char** argv) {
+//	src = imread("C:/Users/xk/Desktop/hand2.png");
+//	if (!src.data) {
+//		printf("could not load image...\n");
+//		return -1;
+//	}
+//	const char* input_win = "input image";
+//	namedWindow(input_win, CV_WINDOW_AUTOSIZE);
+//	namedWindow(output_win, CV_WINDOW_NORMAL);
+//	const char* trackbar_label = "Threshold : ";
+//
+//	cvtColor(src, src_gray, CV_BGR2GRAY);
+//	blur(src_gray, src_gray, Size(3, 3), Point(-1, -1), BORDER_DEFAULT);
+//	imshow(input_win, src_gray);
+//
+//	createTrackbar(trackbar_label, output_win, &threshold_value, threshold_max, Threshold_Callback);
+//	Threshold_Callback(0, 0);
+//	waitKey(0);
+//	return 0;
+//}
+//
+//void Threshold_Callback(int, void*) {
+//	Mat bin_output;
+//	vector<vector<Point>> contours;
+//	vector<Vec4i> hierachy;
+//
+//	threshold(src_gray, bin_output, threshold_value, threshold_max, THRESH_BINARY);
+//	findContours(bin_output, contours, hierachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+//
+//	vector<vector<Point>> convexs(contours.size());
+//	for (size_t i = 0; i < contours.size(); i++) {
+//		convexHull(contours[i], convexs[i], false, true);//计算出图像的凸包，根据图像的轮廓点，通过函数convexhull转化成凸包的点点坐标
+//	}
+//
+//	// 绘制
+//	dst = Mat::zeros(src.size(), CV_8UC3);
+//	vector<Vec4i> empty(0);
+//	for (size_t k = 0; k < contours.size(); k++) {
+//		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//		drawContours(dst, contours, k, color, 2, LINE_8, hierachy, 0, Point(0, 0));
+//		drawContours(dst, convexs, k, color, 2, LINE_8, empty, 0, Point(0, 0));
+//	}
+//	imshow(output_win, dst);
+//
+//	return;
+//}
 
 
 
